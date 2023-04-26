4f899dc50fa0fad369fa7b14758362d0a2772219
mhyavas <mhyavas@gmail.com>
2023-04-26 18:37:13 -0400

    d16 uploaded

--- src/data_reconstructions/d16.clj
+++ src/data_reconstructions/d16.clj
@@ -1,4 +1,6 @@
-(ns data-reconstructions.d16)
+(ns data-reconstructions.d16
+  (:require
+    [clojure.string :as str]))
 ;Tarih: 20230426
 ;
 ;  Girdi:
@@ -28,6 +30,33 @@
           2 {:id 2 :city "Ankara" :name {:first "batu" :last "can"}}})
   (def x1 {:id 2 :city "Ankara" :name {:first "batu" :last "can"}})
 
-  
+  (get-in x1 [:city])
+  ;=> "Ankara"
+  (str/includes? (get-in x1 [:city]) "Ankara")
+  ;=> true
+  (defn f1 [m]
+    (str/includes? (m :city) "Ankara")
+    )
+  (vals x)
+  ;=>
+  ;({:id 1, :city "Istanbul", :name {:first "ali", :last "veli"}}
+  ; {:id 2, :city "Ankara", :name {:first "batu", :last "can"}})
+
+  (map f1 (vals x))
+  ;=> (false true)
+  (defn f2 [m]
+    (if (str/includes? (m :city) "Ankara")
+      m
+      nil))
+
+  (map f2 (vals x))
+  ;=> (nil {:id 2, :city "Ankara", :name {:first "batu", :last "can"}})
+  (remove nil? (map f2 (vals x)))
+  ;=> ({:id 2, :city "Ankara", :name {:first "batu", :last "can"}})
+  (into [] (remove nil? (map f2 (vals x))))
+  ;=> [{:id 2, :city "Ankara", :name {:first "batu", :last "can"}}]
+
+
+
   ;end
-,)
+  )
