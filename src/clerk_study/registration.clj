378cb40f1f555be01dea2da19cd2fd8cf53ecd11
mhyavas <mhyavas@gmail.com>
2023-05-08 13:18:34 -0400

    student registration simulation

--- src/clerk_study/registration.clj
+++ src/clerk_study/registration.clj
@@ -22,9 +22,6 @@
    {:db/ident :title
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
-   {:db/ident :students
-    :db/valueType :db.type/tuple
-    :db/cardinality :db.cardinality/one}
    ])
 ; [rfr](https://docs.datomic.com/pro/schema/schema.html)
 ^{::clerk/visibility {:code :hide}}
@@ -37,14 +34,13 @@
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
    {:db/ident :department/id
-    :db/valueType :db.type/tuple
-    :db/tupleAttrs  []
+    :db/valueType :db.type/ref
     :db/cardinality :db.cardinality/one}
    ])
 (def db
-  {:department {101 {:id 101 :title "Matematik" :students []}
-                102 {:id 102 :title "Fizik" :students []}
-                103 {:id 103 :title "Sosyoloji" :students []}}
+  {:department {101 {:id 101 :title "Matematik"}
+                102 {:id 102 :title "Fizik"}
+                103 {:id 103 :title "Sosyoloji"}}
    :student {}})
 
 ;String doesn't have good look at the clerk table. We need the data in a vector.
@@ -93,21 +89,93 @@
 (def db2 (-> db
      (assoc-in [:student] record2)
      ))
-;{:department {101 {:id 101, :title "Matematik", :students []},
-;              102 {:id 102, :title "Fizik", :students []},
-;              103 {:id 103, :title "Sosyoloji", :students []}},
+;{:department {101 {:id 101, :title "Matematik"},
+;              102 {:id 102, :title "Fizik"},
+;              103 {:id 103, :title "Sosyoloji"}},
 ; :student {:id [1 2], :name ["Can Ali" "Ali Deniz"], :department/id [101 102]}}
 
 (clerk/table (db2 :student))
 
 #_(d/transact conn {:tx-data schema-2})
 ;:db.error/invalid-install-attribute First error: :db.error/invalid-tuple-kind
-#_(d/transact conn {:tx-data schema-2})
+(d/transact conn {:tx-data schema-2})
 ;:db.error/invalid-tuple-value Invalid tuple value
 
 ;İlk hata     :db/tupleAttrs  [:department/id] satırını ekledikten sonra ikinci hataya dönüştü. Satırı aşağıdaki linkten dolayı ekledim
 
 ;q1: [soru linki](https://forum.datomic.com/t/troubles-with-upsert-on-composite-tuples/1355/6)
 
+; ### Course Table
 
-(get-in db2 [:student :id])
+(def cform1 {:id 201 :code "MAT101" :name "Matematik" :department/id [{:id 101 :title "Matematik"} {:id 102 :title "Fizik"}]})
+(def cform2 {:id 301 :code "BIL101" :name "Matlab Giriş" :department/id [{:id 102 :title "Fizik"}]})
+
+(concat cform1 cform2)
+;=>
+;([:id 201]
+; [:code "MAT101"]
+; [:name "Matematik"]
+; [:department/id [{:id 101, :title "Matematik"} {:id 102, :title "Fizik"}]]
+; [:id 301]
+; [:code "BIL101"]
+; [:name "Matlab Giriş"]
+; [:department/id [{:id 101, :title "Matematik"} {:id 102, :title "Fizik"}]])
+
+(def cforms (concat cform1 cform2))
+
+(clerk/table cforms)
+
+;Concat fonksiyonu ile ders formlarini birlestirince tuple dondugunden output istedigimiz gibi olmadi.
+
+(def ct {:id [] :code [] :name [] :department/id []})
+(to-clerk (dissoc cform1 :department/id))
+;=> {:id 201, :code ["MAT101"], :name ["Matematik"]}
+
+(comment
+  (defn f1 [m]
+   (get-in m [:department/id :title]))
+ (map f1 cform1)
+ ;=> (nil nil nil nil)
+ (get cform1 :department/id)
+ ;=> [{:id 101, :title "Matematik"} {:id 102, :title "Fizik"}]
+
+ (map identity (f1 cform1))
+  (defn f2 [m]
+    (get m :department/id)
+    )
+  (map identity (f2 cform1))
+  ;=> ({:id 101, :title "Matematik"} {:id 102, :title "Fizik"})
+  (defn f3 [m]
+    (get m :title))
+  (map f3 (f2 cform1))
+  ;=> ("Matematik" "Fizik")
+
+  ;end
+ ,)
+
+(defn f2 [m]
+  (get m :department/id)
+  )
+(defn f3 [m]
+  (get m :title))
+(def ct1 (-> ct
+             (update-in [:id] conj (cform1 :id))
+             (update-in [:code] conj (cform1 :code))
+             (update-in [:name] conj (cform1 :name))
+             (update-in [:department/id] conj (map f3 (f2 cform1)))))
+
+(clerk/table ct1)
+
+(def ct2 (-> ct1
+             (update-in [:id] conj (cform2 :id))
+             (update-in [:code] conj (cform2 :code))
+             (update-in [:name] conj (cform2 :name))
+             (update-in [:department/id] conj (map f3 (f2 cform2)))))
+
+
+; ### Course List Table
+^{::clerk/visibility {:code :hide}}
+(clerk/table ct2)
+
+
+
---
+++ src/clerk_study/registration_2.clj
@@ -0,0 +1,126 @@
+(ns clerk-study.registration-2
+  (:require [nextjournal.clerk :as clerk]))
+
+^{::clerk/visibility {:code :hide}}
+
+(require '[clojure.walk :refer [postwalk]])
+^{::clerk/visibility {:code :hide}}
+
+(require '[datomic.client.api :as d])
+; ## Creating auto-increment id
+
+^{::clerk/visibility {:code :hide}}
+(def st {:id [] :name [] :department/id []})
+
+^{::clerk/visibility {:code :hide}}
+(clerk/table st)
+
+(def form1 {:id 1 :name "Can Ali" :department/id [[:id 101 :title "Matematik"]]})
+(def form2 {:name "Ali Deniz" :department/id [[:id 102 :title "Fizik"] [:id 101 :title "Matematik"]]})
+(def form3 {:name "Mahmut Can" :department/id [[:id 103 :title "Sosyoloji"]]})
+(def form4 {:name "Nazım Taş" :department/id [[:id 101 :title "Matematik"]]})
+^{::clerk/visibility {:code :hide}}
+(comment
+
+  (def record1 (-> st
+                   (update-in [:id] conj (form1 :id))
+                   (update-in [:name] conj (form1 :name))
+                   (update-in [:department/id] conj (form1 :department/id))))
+  (identity record1)
+  ;=> {:id [nil], :name ["Can Ali"], :department/id [101]}
+
+  (apply inc (record1 :id))
+  ;=> 2
+  (def record2 (-> record1
+               (update-in [:id] conj (apply inc (record1 :id)))
+               (update-in [:name] conj (form2 :name))
+               (update-in [:department/id] conj (form2 :department/id))
+               ))
+  (identity record2)
+  ;=> {:id [1 2], :name ["Can Ali" "Ali Deniz"], :department/id [101 102]}
+  (last (record2 :id))
+  ;=> 2
+  (def record3 (-> record2
+                   (update-in [:id] conj (inc (last (record2 :id))))
+                   (update-in [:name] conj (form3 :name))
+                   (update-in [:department/id] conj (form3 :department/id))
+                   ))
+
+  (identity record3)
+  ;=> {:id [1 2 3], :name ["Can Ali" "Ali Deniz" "Mahmut Can"], :department/id [101 102 103]}
+
+  ;After changing department id formation from ":id 101" to [[:id 101 :title "Matematik"]]
+
+  (form3 :department/id)
+  :=> [[:id 103 :title "Sosyoloji"]]
+
+  (map identity (form2 :department/id))
+  ;=> ([:id 102 :title "Fizik"] [:id 101 :title "Matematik"])
+
+  (defn f1 [xs]
+    (last xs)
+    )
+
+  (map f1 (form2 :department/id))
+  ;=> ("Fizik" "Matematik")
+
+  (def record5 (-> st
+                 (update-in [:id] conj (form1 :id))
+                 (update-in [:name] conj (form1 :name))
+                 (update-in [:department/id] conj (map f1 (form2 :department/id)))))
+  (identity record5)
+  ;=> {:id [1], :name ["Can Ali"], :department/id [("Fizik" "Matematik")]}
+  (def record6 (-> record5
+                   (update-in [:id] conj (inc (last (record5 :id))))
+                   (update-in [:name] conj (form3 :name))
+                   (update-in [:department/id] conj (map f1 (form3 :department/id))) ))
+
+  (identity record6)
+  => {:id [1 2], :name ["Can Ali" "Mahmut Can"], :department/id [("Fizik" "Matematik") ("Sosyoloji")]}
+  ;
+  (def record4 (-> record3
+                   (update-in [:id] conj (inc (last (record3 :id))))
+                   (update-in [:name] conj (form4 :name))
+                   (update-in [:department/id] conj (form4 :department/id))
+                   ))
+  (identity record4)
+  ;=> {:id [1 2 3 4], :name ["Can Ali" "Ali Deniz" "Mahmut Can" "Nazım Taş"], :department/id [101 102 103 101]}
+  ;end
+)
+
+(def record1 (-> st
+                 (update-in [:id] conj (form1 :id))
+                 (update-in [:name] conj (form1 :name))
+                 (update-in [:department/id] conj (form1 :department/id))))
+
+
+(def record2 (-> record1
+                 (update-in [:id] conj (inc (last(record1 :id))))
+                 (update-in [:name] conj (form2 :name))
+                 (update-in [:department/id] conj (form2 :department/id))
+                 ))
+
+(def record3 (-> record2
+                 (update-in [:id] conj (inc (last (record2 :id))))
+                 (update-in [:name] conj (form3 :name))
+                 (update-in [:department/id] conj (form3 :department/id))
+                 ))
+
+(clerk/table record3)
+
+
+; ### After changing formation of the departments
+(defn f1 [xs]
+  (last xs)
+  )
+(def record5 (-> st
+                 (update-in [:id] conj (form1 :id))
+                 (update-in [:name] conj (form1 :name))
+                 (update-in [:department/id] conj (map f1 (form2 :department/id)))))
+(def record6 (-> record5
+                 (update-in [:id] conj (inc (last (record5 :id))))
+                 (update-in [:name] conj (form3 :name))
+                 (update-in [:department/id] conj (map f1 (form3 :department/id))) ))
+
+(clerk/table record6)
+
