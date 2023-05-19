b43b04c58cdd3b296db517a7a4b71b3d713566b5
mhyavas <mhyavas@gmail.com>
2023-05-19 09:33:22 -0400

    Working on Datomic

--- src/clerk_study/reg_sim/e02.clj
+++ src/clerk_study/reg_sim/e02.clj
@@ -28,9 +28,67 @@
 
 
 (d/q
-  '[:find ?e ?product-name ?color ?product-price
-    :in $ [[?product-name ?product-price]]
+  '[:find ?e ?course-name ?department
+    :in $ [[?course-name]]
     :where
-    [?e :product/name ?product-name]
-    [?e :product/color ?color]]
-  db [["Kalem" 120] ["Defter" 250]])
+    [?e :course/name ?course-name]
+    [?e :course/department ?department]]
+  datodb [["Matlab Giriş"] ["Matematik"]])
+;=> [[92358976733264 "Matlab Giriş" 200]
+; [92358976733263 "Matematik" 200]
+; [92358976733263 "Matematik" 100]]
+
+
+(d/q
+  '[:find ?e ?course-name ?department ?course-code
+    :in $ [[?course-name]]
+    :where
+    [?e :course/name ?course-name]
+    [?e :course/department ?department]
+    [?e :course/code ?course-code]]
+  datodb [["Matlab Giriş"] ["Matematik"]])
+;=>
+;[[92358976733263 "Matematik" 200 "MAT101"]
+; [92358976733264 "Matlab Giriş" 200 "BIL101"]
+; [92358976733263 "Matematik" 100 "MAT101"]]
+
+
+
+#_(d/q
+  '[:find (pull ?e [*])
+    :where
+    [?e :department/id _]]
+  datodb)
+;[[{:db/id 92358976733259, :department/id 100, :department/title "Matematik"}]
+; [{:db/id 92358976733260, :department/id 200, :department/title "Fizik"}]
+; [{:db/id 92358976733261, :department/id 300, :department/title "Sosyoloji"}]]
+
+;q1: Referans olarak department/id kullandik. Sorgu ekrani problemini cozerken programlama tarafinda yapmak durumunda
+;kalmamiz gerekiyor gibi geldi.
+;
+(def departments (-> (d/q
+                       '[:find (pull ?e [*])
+                         :where
+                         [?e :department/id _]]
+                       datodb)
+                     flatten) )
+
+(identity departments)
+;=>
+;({:db/id 92358976733259, :department/id 100, :department/title "Matematik"}
+; {:db/id 92358976733260, :department/id 200, :department/title "Fizik"}
+; {:db/id 92358976733261, :department/id 300, :department/title "Sosyoloji"})
+
+(def dt {:id [] :title []})
+
+(map #(identity (% :department/id)) departments)
+;=> (100 200 300)
+
+
+(into [] #(% :department/id) departments)
+(map #(assoc-in dt [:id] (% :department/id)) departments)
+;=> ({:id 100, :title []} {:id 200, :title []} {:id 300, :title []})
+(-> dt
+    (map #(assoc-in (% :department/id)) departments))
+    (assoc-in [:title] (departments :title)))
+(clerk/table departments)
